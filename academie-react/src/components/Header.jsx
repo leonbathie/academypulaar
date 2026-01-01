79b9af2cf4f3f33c8835d8a3dbf799fe628733a2
Ousmane BA <ousmane.ba42@unchk.edu.sn>
2026-01-01 02:35:01 +0000

    Fix: ajout import logoImage dans Header.jsx

--- academie-react/src/components/Header.jsx
+++ academie-react/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { useTheme } from '../context/ThemeContext'
-
+import logoImage from '../assets/logo-academie.png'
 import './Header.css'
 
 const languages = [
