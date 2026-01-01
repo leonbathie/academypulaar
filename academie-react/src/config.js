3ec4080e87ad9c1bdac085b5458f30f9b1fd7841
Ousmane BA <ousmane.ba42@unchk.edu.sn>
2026-01-01 14:46:47 +0000

    Fix double /api dans URL

--- academie-react/src/config.js
+++ academie-react/src/config.js
@@ -2,5 +2,5 @@
 // En production, utilisez la variable d'environnement VITE_API_URL
 const isProduction = window.location.hostname !== 'localhost';
 export const API_URL = isProduction 
-    ? 'https://goomufulowidto.servemp3.com/api' 
+    ? 'https://goomufulowidto.servemp3.com' 
     : 'http://localhost:5000'
