92c2b220280ddb8e797e841b514081a31c192b62
Ousmane BA <ousmane.ba42@unchk.edu.sn>
2026-01-27 20:51:40 +0000

    Fix API URL - utiliser IP en production

--- academie-react/src/config.js
+++ academie-react/src/config.js
@@ -1,6 +1,9 @@
 // Configuration API centralisée
 // En production, utilisez la variable d'environnement VITE_API_URL
 const isProduction = window.location.hostname !== 'localhost';
+const hostname = window.location.hostname;
+
+// Utiliser l'IP si on est sur l'IP, sinon utiliser le domaine
 export const API_URL = isProduction 
-    ? 'https://goomunfulawidto.serveblog.net' 
+    ? (hostname === '173.249.22.217' ? 'https://173.249.22.217' : 'https://goomunfulawidto.serveblog.net')
     : 'http://localhost:5000'
