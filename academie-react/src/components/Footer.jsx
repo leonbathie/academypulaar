1b922593c8ab141d4b8e50024f9b2564ccc20865
Ousmane BA <ousmane.ba42@unchk.edu.sn>
2026-03-15 15:09:13 +0000

    fix: traduire 'Contact' en Fulfulde (Jokkorde) dans le footer
    - Footer.jsx: remplacer texte en dur par t('footer.contactTitle')
    - ff.json: ajouter contactTitle = Jokkorde
    - fr.json: ajouter contactTitle = Contact
    - en.json: ajouter contactTitle = Contact

--- academie-react/src/components/Footer.jsx
+++ academie-react/src/components/Footer.jsx
@@ -67,7 +67,7 @@ function Footer() {
 
                         {/* Contact & Social Column */}
                         <div className="footer-contact-col">
-                            <h4 className="footer-title">Contact</h4>
+                            <h4 className="footer-title">{t('footer.contactTitle')}</h4>
                             <div className="footer-contact-info">
                                 <div className="contact-item">
                                     <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" /><circle cx="12" cy="10" r="3" /></svg>
