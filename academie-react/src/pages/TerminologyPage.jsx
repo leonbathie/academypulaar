07b88b432c952dc827bb32d823b53f2f15e6651c
Ousmane BA <ousmane.ba42@unchk.edu.sn>
2026-04-06 01:24:09 +0000

    fix: supprimer le bouton CTA dictionnaire de la page terminologie

--- academie-react/src/pages/TerminologyPage.jsx
+++ academie-react/src/pages/TerminologyPage.jsx
@@ -49,19 +49,6 @@ function TerminologyPage() {
                         ))}
                     </div>
                 </section>
-
-                {/* CTA */}
-                <section className="term-cta">
-                    <h2>{t('terminology.explore')}</h2>
-                    <p>{t('terminology.exploreText')}</p>
-                    <Link to="/dictionnaire" className="btn-cta">
-                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                            <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" />
-                            <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" />
-                        </svg>
-                        {t('nav.dictionary')}
-                    </Link>
-                </section>
             </div>
         </div>
     )
